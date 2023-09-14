1ee365f17a26fa4fdfb3910d577fc03f225c81bd
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-14 20:12:50 +0000

    Adding the necessary libray for the sketches

--- Arduino Projects/automatic_room_light_controller/automatic_room_light_controller.ino
+++ Arduino Projects/automatic_room_light_controller/automatic_room_light_controller.ino
@@ -1,15 +1,13 @@
 
 #include <SoftwareSerial.h>
-// #include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>
 
 
 
-// LiquidCrystal_I2C lcd(0x27,16,2);  
+LiquidCrystal_I2C lcd(0x27,16,2);  
 
 //GSM Module TX is connected to Arduino D8
 #define PIR_SENSOR_1 7
-#define PIR_SENSOR_2 7
-#define PIR_SENSOR_3 7
 
 
 //GSM Module RX is connected to Arduino D9
@@ -18,7 +16,7 @@
 
 SoftwareSerial mySerial(SIM800_TX_PIN, SIM800_RX_PIN); //Create software serial object to communicate with GSM Module
 
-int device_1 = 7; // attach pin D13 of Arduino to Device-1
+int device_1 = 2; // attach pin D13 of Arduino to Device-1
 
 // defines variables
 int index = 0;
@@ -40,17 +38,17 @@ void setup()
   pinMode(PIR_SENSOR_3, INPUT);//Sets the pir sensor as an INPUT
 
   digitalWrite(device_1, LOW); //Sets the device_1 in to OFF state at the beginning
-  //  lcd.init();  //initialized LCD 
-  // lcd.clear();         
-  // lcd.backlight(); //turn on the backlight of the lcd
+   lcd.init();  //initialized LCD 
+  lcd.clear();         
+  lcd.backlight(); //turn on the backlight of the lcd
 
   // Check if you're currently connected to SIM800L
   while (!mySerial.available()) {
     mySerial.println("AT");
     delay(1000);
     Serial.println("connecting....");
-    // lcd.connecting...");setCursor(2,0); 
-    // lcd.print("connecting...");
+    lcd.connecting...");setCursor(2,0); 
+    lcd.print("connecting...");
   }
 
   Serial.println("Connected..");
@@ -63,10 +61,10 @@ void setup()
 
   Serial.println("Ready to received Commands..");
    for (int position = 0; position < 16; position++) {
-    // lcd.scrollDisplayLeft();
+    lcd.scrollDisplayLeft();
     delay(300);  // Adjust this delay to control scrolling speed
-    // lcd.setCursor(0,1); 
-    // lcd.print("Ready to received Commands..");
+    lcd.setCursor(0,1); 
+    lcd.print("Ready to received Commands..");
   }
 
 }
@@ -109,9 +107,9 @@ void receivedMessage(String inputString) {
   index = inputString.indexOf('"');
   number = inputString.substring(0, index);
   Serial.println("Number: " + number);
-  // lcd.clear();
-  // lcd.setCursor(2, 1);
-  // lcd.print( number);
+  lcd.clear();
+  lcd.setCursor(2, 1);
+  lcd.print( number);
 
   //Get The Message of the sender
   index = inputString.indexOf("\n") + 1;
@@ -126,49 +124,49 @@ void receivedMessage(String inputString) {
     digitalWrite(device_1, LOW);
     delay(1000);
     Serial.println("LIGHT ON");
-    // lcd.setCursor(3,0);
-    // lcd.print("LIGHT ON");
+    lcd.setCursor(3,0);
+    lcd.print("LIGHT ON");
   }
 
   //turn Device 1 OFF
   if (message.indexOf("STOP") > -1 ) {
     digitalWrite(device_1, HIGH);
     Serial.println("LIGHT OFF");
-    // lcd.setCursor(3,0);
-    // lcd.print("LIGHT OFF ");
+    lcd.setCursor(3,0);
+    lcd.print("LIGHT OFF ");
   }
 
   delay(10);// Added delay between two readings
 }
 
-// void pir_sensor_message()
-// {
-//   int motion1 = digitalRead(PIR_SENSOR_1);// Declaring the the reading of the sensors
-//   int motion2 = digitalRead(PIR_SENSOR_2);
-//   int motion3 = digitalRead(PIR_SENSOR_3);
+void pir_sensor_message()
+{
+  int motion1 = digitalRead(PIR_SENSOR_1);// Declaring the the reading of the sensors
+  int motion2 = digitalRead(PIR_SENSOR_2);
+  int motion3 = digitalRead(PIR_SENSOR_3);
 
-//   if (motion1 == HIGH) {
-//         digitalWrite(device_1, LOW);
-//     Serial.println("Motion detected by sensor 1");
-//     // delay(200000);
-//     // Perform actions for sensor 1 motion detection
-//   }
+  if (motion1 == HIGH) {
+        digitalWrite(device_1, LOW);
+    Serial.println("Motion detected by sensor 1");
+    // delay(200000);
+    // Perform actions for sensor 1 motion detection
+  }
 
-//    else if (motion2 == HIGH) {
-//     digitalWrite(device_1, LOW);
-//     Serial.println("Motion detected by sensor 2");
-//     // delay(200000);
-//     // Perform actions for sensor 2 motion detection
-//   }
+   else if (motion2 == HIGH) {
+    digitalWrite(device_1, LOW);
+    Serial.println("Motion detected by sensor 2");
+    // delay(200000);
+    // Perform actions for sensor 2 motion detection
+  }
 
-//   else if (motion3 == HIGH) {
-//     digitalWrite(device_1, LOW);
-//     Serial.println("Motion detected by sensor 3");
-//     // delay(200000);
-//     // Perform actions for sensor 3 motion detection
-//    }
-//    delay(1000);
-// }
+  else if (motion3 == HIGH) {
+    digitalWrite(device_1, LOW);
+    Serial.println("Motion detected by sensor 3");
+    // delay(200000);
+    // Perform actions for sensor 3 motion detection
+   }
+   delay(1000);
+}
 
 
 
