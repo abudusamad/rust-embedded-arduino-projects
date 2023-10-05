a5219db285478e220d20960fb9185cae9c4cf86e
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-05 19:38:04 +0000

    modified the comment section for the projects

--- Esp8266/ESP12/Programming/Programming.ino
+++ Esp8266/ESP12/Programming/Programming.ino
@@ -1,11 +1,3 @@
-/*
-
-  ESP12/Programming
-  The basics of programming an ESP-12 with the ESP8266 core for Arduino
-
-  For info and circuit diagrams see https://github.com/tardate/LittleArduinoProjects/tree/master/ESP8266/ESP12/Programming
-
- */
 
 const int PINS[] = {
   2,
