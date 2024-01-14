971f537a2bc46b78eb990a1db39b53d2040c6c27
abudusamad <abudusamed@gmail.com>
2024-01-14 23:50:05 +0000

    making the sum of the number

---
+++ computer-graphics/basic001/hello/sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    float number1, number2, sum, average;
+
+    cout << "Enter two numbers: ";
+    cin >> number1;
+    cin >> number2;
+
+    sum = number1 + number2;
+
+    average = sum / 2;
+
+    cout << "Sum = " << sum << endl;
+    cout << "Average = " << average << endl;
+    return 0;
+}
