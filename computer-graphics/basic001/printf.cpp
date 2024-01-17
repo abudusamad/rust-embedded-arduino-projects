7e4c060169573d230c0dae5521970e0f906221c7
abudusamad <abudusamed@gmail.com>
2024-01-17 23:50:49 +0000

    Adding graphics to the page

---
+++ computer-graphics/basic001/printf.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = a + b = " << c << endl;
+
+    return 0;
+}
